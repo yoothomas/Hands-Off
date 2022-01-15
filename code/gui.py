99ade3c18da31aec1add61b5c7ac6ac7453f81bd
Thomas Yoo <thomas.y1214@gmail.com>
2022-01-15 17:19:40 -0500

    laptop switch

--- code/gui.py
+++ code/gui.py
@@ -7,16 +7,32 @@ class GUI(object):
         #Create window
         self.window = tk.Tk()
         self.window.title("###PROJECTNAME")
+        
+        self. _base_window()
+        self._welcome_screen()
+    
+        self.update()
+
+        self.session = ""
+
+    def _base_window(self):
+
         self.window.resizable(1,1)
         self.window.minsize(600,450)
         self.frame = tk.Frame()
         self.popup = ""
         self.state = "Init"
 
+    def _welcome_screen(self):
 
-        self.update()
+        self.frame.destroy()
 
-        self.session = ""
+        self._base_window()
+
+        title = tk.Label(self.frame, width=50, text="Welcome to Project!")
+        register 
+
+        
 
     #Mainloop GUI
     def loop(self):
